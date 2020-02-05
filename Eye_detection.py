cd1920e63cf9abadcc6abc0ee2ad7e58b0310cfc
JAYA SURYA M <57759507+uknwho@users.noreply.github.com>
2020-02-05 18:54:21 +0530

    Eye Detection

---
+++ Eye_detection.py
@@ -0,0 +1,22 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+
+cap = cv2.VideoCapture(0)
+
+while cap.isopened():
+    _, img = cap.read()
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces= face_cascade.detectMultiScale(gray,1.1,4)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y),(x+w,y+h), (0,0,255),3)
+        roi_gray=gray[x:x+w,y:y+h]
+        roi_img=img[x:x+w,y:y+h]
+        eyes =eye_cascade.detectMultiScale(roi_gray)
+        for(ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_img,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
+    cv2.imshow('img',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
