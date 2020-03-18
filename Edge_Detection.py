3ae92e0533d26d0c843893cd4b0267168df601b9
JAYA SURYA M <57759507+uknwho@users.noreply.github.com>
2020-03-19 02:02:35 +0530

    Update and rename edge.py to Edge_Detection.py

--- Edge_Detection.py
+++ Edge_Detection.py
@@ -1,4 +1,4 @@
-#Edge Detection using Sobel
+#Edge Detection using Laplacian and Sobel Methods
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -7,19 +7,19 @@ img=cv2.imread('building.jpg')
 h=img.shape[0]
 w=img.shape[1]
 print(h,w)
+
 img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 lap=cv2.Laplacian(img,cv2.CV_64F)
-lap=np.uint8(np.absolute(lap))
-
 sobelX=cv2.Sobel(img,cv2.CV_64F,1,0)
 sobelY=cv2.Sobel(img,cv2.CV_64F,0,1)
 
+lap=np.uint8(np.absolute(lap))
 sobelX=np.uint8(np.absolute(sobelX))
 sobelY=np.uint8(np.absolute(sobelY))
-
 sobelC=cv2.bitwise_or(sobelX,sobelY)
-titles=['Image','Laplacian','sobelX','sobelY','sobelC']
+
+titles=['Image','Laplacian','SobelX','SobelY','SobelC']
 images=[img,lap,sobelX,sobelY,sobelC]
 
 for i in range(5):
