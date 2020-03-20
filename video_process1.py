b434f636bb6ae58ffaa6db0de1e100aaaae5c563
JAYA SURYA M <57759507+uknwho@users.noreply.github.com>
2020-03-20 18:32:03 +0530

    Update video_process1.py

--- video_process1.py
+++ video_process1.py
@@ -1,8 +1,9 @@
-import cv2
-cap=cv2.VideoCapture(0);
-out=cv2.VideoWriter_fourcc(*'XVID')
-vid=cv2.VideoWriter('vid2.avi',out,20,(640,480));
+#! usr/bin/env python
 
+import cv2
+cap=cv2.VideoCapture(0);  # takes the feed from the laptop cam.
+out=cv2.VideoWriter_fourcc(*'XVID') # video format.
+vid=cv2.VideoWriter('vid2.avi',out,20,(640,480)); # video saves to vid2.avi
 
 while(cap.isOpened()):
     ret, frame=cap.read();
