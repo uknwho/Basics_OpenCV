c2c777dc39dfe20560007c61f188f7a136b2e87f
JAYA SURYA M <57759507+uknwho@users.noreply.github.com>
2020-04-12 19:10:20 +0530

    Update Thresholds.py

--- Thresholds.py
+++ Thresholds.py
@@ -1,4 +1,7 @@
+# In Thresholding, every pixcel of the image is compared with a threshold value and converted to 0 and 1. It is a sementation technique to seperate an object as foreground and backgrounding the other unimportant spaces.
+# Thresholding is done only for grayscale images. In this code, different Thresholding methods are employed on a single image and the output is saved in "Thresholds.png" to understand each method.
 #! usr/bin/env python
+
 
 import cv2
 from matplotlib import pyplot as plt
