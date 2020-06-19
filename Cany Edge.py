757df1f11595e25eb197a100adb3f3aabd3b42bb
JAYA SURYA M <57759507+uknwho@users.noreply.github.com>
2020-06-19 22:08:34 +0530

    Update Cany Edge.py

--- Cany Edge.py
+++ Cany Edge.py
@@ -1,9 +1,13 @@
 # Edge Detection using Cany Edge Method
 
+# importing Libraries
 import cv2 as cv2
 from matplotlib import pyplot as plt
 
-img=cv2.imread('lena.jpg')  # which ever image you want to detect edges.
+# Read the image which ever image you want to detect edges.
+img=cv2.imread('lena.jpg') 
+
+# Preprocessing the image and applying the canny edge detector.
 img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB  )
 cany=cv2.Canny(img,100,200)
 
